73ca75d66c3f344eee655901566dd70cef1b81fa
Dav <dibadgithub@gmail.com>
2018-10-23 11:53:08 +0100

    Chapter 4, Red Circle

--- vec3.h
+++ vec3.h
@@ -42,3 +42,55 @@ public:
   float e[3];
 };
 
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+  is >> t.e[0] >> t.e[1] >> t.e[2];
+  return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+  os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+  return os;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline vec3 operator*(float t, const vec3 &v) {
+  return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator/(vec3 v, float t) {
+  return vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
+}
+
+inline vec3 operator*(const vec3 &v, float t) {
+  return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+  return v1.e[0]*v2.e[0] + v1.e[1]*v2.e[1] + v1.e[2]*v2.e[2];
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+  return vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+              (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+              (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+}
+
+inline vec3 unit_vector(vec3 v) {
+  return v / v.length();
+}

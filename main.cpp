c257cd18010ad805bf2e865ed1d1647932cdb30e
Dav <dibadgithub@gmail.com>
2018-10-24 11:41:13 +0100

    Fixed problem with normal

--- main.cpp
+++ main.cpp
@@ -3,23 +3,30 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2.0f * dot(oc, r.direction());
   float c = dot(oc, oc) - radius*radius;
   float discriminant = b*b - 4*a*c;
-  return (discriminant > 0);
+
+  if(discriminant < 0)
+    return -1.0f;
+  else
+    return (-b - sqrt(discriminant)) / (2.0f*a);
 }
 
 vec3 color(const ray& r)
 {
-  if(hit_sphere(vec3(0, 0, -1), 0.5f, r))
-    return vec3(1, 0, 0);
-
+  float t = hit_sphere(vec3(0, 0, -1), 0.5f, r);
+  if(t > 0.0f)
+  {
+    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+    return 0.5f * vec3(N.x()+1, N.y()+1, N.z()+1);
+  }
   vec3 unit_direction = unit_vector(r.direction());
-  float t = 0.5 * (unit_direction.y() + 1.0f);
+  t = 0.5 * (unit_direction.y() + 1.0f);
   return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
@@ -35,8 +42,8 @@ int main()
 
   for(int j = ny-1; j >= 0; --j) {
     for(int i = 0; i < nx; ++i) {
-      float u = (float)i / (float)nx;
-      float v = (float)j / (float)ny;
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
 
       ray r(origin, lower_left_corner + u*horizontal + v*vertical);
       vec3 col = color(r);
--- vec3.h
+++ vec3.h
@@ -53,6 +53,13 @@ inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
   return os;
 }
 
+inline void vec3::make_unit_vector() {
+  float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+}
+
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
   return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
@@ -89,6 +96,49 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
   return vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
               (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
               (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+}
+
+inline vec3& vec3::operator+=(const vec3 &v) {
+  e[0] += v.e[0];
+  e[1] += v.e[1];
+  e[2] += v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v) {
+  e[0] *= v.e[0];
+  e[1] *= v.e[1];
+  e[2] *= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v) {
+  e[0] /= v.e[0];
+  e[1] /= v.e[1];
+  e[2] /= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3 &v) {
+  e[0] -= v.e[0];
+  e[1] -= v.e[1];
+  e[2] -= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator*=(const float t) {
+  e[0] *= t;
+  e[1] *= t;
+  e[2] *= t;
+  return *this;
+}
+
+inline vec3& vec3::operator/=(const float t) {
+  float k = 1.0/t;
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+  return *this;
 }
 
 inline vec3 unit_vector(vec3 v) {

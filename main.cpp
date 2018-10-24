1517835a90b8ac457985084d80abb76d03937e37
Dav <dibadgithub@gmail.com>
2018-10-24 14:34:56 +0100

    Diffuse materials

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@
 #include "hitablelist.h"
 #include "camera.h"
 
+vec3 random_in_unit_sphere()
+{
+  vec3 p;
+  do
+  {
+    p = 2.0f*vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while(p.squared_length() >= 1.0f);
+  return p;
+}
+
 vec3 color(const ray& r, hitable *world)
 {
   hit_record rec;
-  if(world->hit(r, 0.0f, FLT_MAX, rec))
-    return 0.5f*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+  if(world->hit(r, 0.001f, FLT_MAX, rec))
+  {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5f*color(ray(rec.p, target-rec.p), world);
+  }
   else
   {
     vec3 unit_direction = unit_vector(r.direction());
@@ -45,6 +58,7 @@ int main()
         col += color(r, world);
       }
       col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99f*col[0]);
       int ig = int(255.99f*col[1]);
       int ib = int(255.99f*col[2]);

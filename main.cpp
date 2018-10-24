1bf52952014580d3dadc7d119b61c3d80c268251
Dav <dibadgithub@gmail.com>
2018-10-24 12:01:05 +0100

    Chapter 5

--- main.cpp
+++ main.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
+#include <cfloat>
 
-#include "vec3.h"
-#include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r)
+vec3 color(const ray& r, hitable *world)
 {
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(), r.direction());
-  float b = 2.0f * dot(oc, r.direction());
-  float c = dot(oc, oc) - radius*radius;
-  float discriminant = b*b - 4*a*c;
-
-  if(discriminant < 0)
-    return -1.0f;
+  hit_record rec;
+  if(world->hit(r, 0.0f, FLT_MAX, rec))
+    return 0.5f*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
   else
-    return (-b - sqrt(discriminant)) / (2.0f*a);
-}
-
-vec3 color(const ray& r)
-{
-  float t = hit_sphere(vec3(0, 0, -1), 0.5f, r);
-  if(t > 0.0f)
   {
-    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-    return 0.5f * vec3(N.x()+1, N.y()+1, N.z()+1);
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0f);
+    return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
   }
-  vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5 * (unit_direction.y() + 1.0f);
-  return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
 int main()
 {
-  int nx = 200;
-  int ny = 100;
+  int nx = 800;
+  int ny = 400;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
   vec3 horizontal(4.0f, 0.0f, 0.0f);
   vec3 vertical(0.0f, 2.0f, 0.0f);
   vec3 origin(0.0f, 0.0f, 0.0f);
+
+  hitable *list[2];
+  list[0] = new sphere(vec3(0, 0, -1), 0.5f);
+  list[1] = new sphere(vec3(0, -100.5, -1), 100);
+  hitable *world = new hitable_list(list, 2);
 
   for(int j = ny-1; j >= 0; --j) {
     for(int i = 0; i < nx; ++i) {
@@ -46,7 +38,8 @@ int main()
       float v = float(j) / float(ny);
 
       ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-      vec3 col = color(r);
+      vec3 p = r.point_at_parameter(2.0f);
+      vec3 col = color(r, world);
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
@@ -54,4 +47,8 @@ int main()
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+
+  delete list[0];
+  delete list[1];
+  delete world;
 }

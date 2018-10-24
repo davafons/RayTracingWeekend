b98e3df58f1aa6a08c9f634020bf4fa84a473973
Dav <dibadgithub@gmail.com>
2018-10-24 14:27:03 +0100

    Antialiasing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "sphere.h"
 #include "hitablelist.h"
+#include "camera.h"
 
 vec3 color(const ray& r, hitable *world)
 {
@@ -19,31 +20,34 @@ vec3 color(const ray& r, hitable *world)
 
 int main()
 {
-  int nx = 800;
-  int ny = 400;
+  int nx = 600;
+  int ny = 300;
+  int ns = 100;
+
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-  vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-  vec3 horizontal(4.0f, 0.0f, 0.0f);
-  vec3 vertical(0.0f, 2.0f, 0.0f);
-  vec3 origin(0.0f, 0.0f, 0.0f);
 
   hitable *list[2];
   list[0] = new sphere(vec3(0, 0, -1), 0.5f);
-  list[1] = new sphere(vec3(0, -100.5, -1), 100);
+  list[1] = new sphere(vec3(0, -100.5f, -1), 100);
   hitable *world = new hitable_list(list, 2);
+
+  camera cam;
 
   for(int j = ny-1; j >= 0; --j) {
     for(int i = 0; i < nx; ++i) {
-      float u = float(i) / float(nx);
-      float v = float(j) / float(ny);
-
-      ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-      vec3 p = r.point_at_parameter(2.0f);
-      vec3 col = color(r, world);
-      int ir = int(255.99*col[0]);
-      int ig = int(255.99*col[1]);
-      int ib = int(255.99*col[2]);
-
+      vec3 col(0, 0, 0);
+      for(int s = 0; s < ns; ++s)
+      {
+        float u = float(i + drand48()) / float(nx);
+        float v = float(j + drand48()) / float(ny);
+        ray r = cam.get_ray(u, v);
+        vec3 p = r.point_at_parameter(2.0f);
+        col += color(r, world);
+      }
+      col /= float(ns);
+      int ir = int(255.99f*col[0]);
+      int ig = int(255.99f*col[1]);
+      int ib = int(255.99f*col[2]);
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }

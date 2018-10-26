9b116c1cbf62c1f7a9974362047b0e949c6d949b
Dav <dibadgithub@gmail.com>
2018-10-26 17:47:44 +0100

    Red sphere

--- main.cpp
+++ main.cpp
@@ -4,17 +4,34 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0f * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius*radius;
+  float discriminant = b*b - 4*a*c;
+
+  return (discriminant > 0.0f);
+}
+
 vec3 color(const ray& r)
 {
   vec3 unit_direction = unit_vector(r.direction());
+
+  // Paint sphere
+  if(hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r))
+    return vec3(1.0f, 0.0f, 0.0f);
+
+  // Background fade
   float t = 0.5f*(unit_direction.y() + 1.0f);
   return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
 void createPpm(const std::string &filename)
 {
-  const int width = 200;
-  const int height = 100;
+  const int width = 400;
+  const int height = 200;
   const int max_rgb_value = 255;
 
   std::ofstream outf;
@@ -39,7 +56,7 @@ void createPpm(const std::string &filename)
         vec3 col = color(r);
         int ir = int(255.99 * col.r());
         int ig = int(255.99 * col.g());
-        int ib = int(255.99 * col.b()); 
+        int ib = int(255.99 * col.b());
         outf << ir << " " << ig << " " << ib << "\n";
       }
     }

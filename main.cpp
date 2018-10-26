e9a495a653788894afb88033ee3118154d020de6
Dav <dibadgithub@gmail.com>
2018-10-26 17:53:31 +0100

    Normal maps

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
@@ -12,19 +12,25 @@ bool hit_sphere(const vec3& center, float radius, const ray& r)
   float c = dot(oc, oc) - radius*radius;
   float discriminant = b*b - 4*a*c;
 
-  return (discriminant > 0.0f);
+  if(discriminant < 0)
+    return -1;
+  else
+    return (-b - sqrt(discriminant)) / (2.0f*a);
 }
 
 vec3 color(const ray& r)
 {
+  // Paint sphere (with normals)
+  float t = hit_sphere(vec3(0, 0, -1), 0.5f, r);
+  if(t > 0.0f)
+  {
+    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+    return 0.5f*vec3(N.x()+1, N.y()+1, N.z()+1);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
-
-  // Paint sphere
-  if(hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r))
-    return vec3(1.0f, 0.0f, 0.0f);
-
   // Background fade
-  float t = 0.5f*(unit_direction.y() + 1.0f);
+  t = 0.5f*(unit_direction.y() + 1.0f);
   return (1.0f-t)*vec3(1.0f, 1.0f, 1.0f) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
